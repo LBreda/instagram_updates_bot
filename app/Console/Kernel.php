d1e05346472c78447d1a9fc91dedfcfa0906277b
Lorenzo <lorenzo@lbreda.com>
2018-10-20 15:34:44 +0200

    :bug: Avoided cron processes overlapping

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('igud:check-send')->everyFifteenMinutes();
+        $schedule->command('igud:check-send')->everyFifteenMinutes()->withoutOverlapping();
     }
 
     /**
