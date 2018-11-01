02d49bf8c71cbdbec16098f5b952828364568f4c
Lorenzo Breda <lorenzo@lbreda.com>
2018-11-01 18:54:42 +0100

    :sparkles: Function to add profiles chatting with the bot

--- app/Http/Controllers/UpdatesController.php
+++ app/Http/Controllers/UpdatesController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\User;
+use App\Traits\InstagramProfileHelper;
 use Telegram\Bot\Laravel\Facades\Telegram;
 
 class UpdatesController extends Controller
 {
+    use InstagramProfileHelper;
 
     /**
      * Webhook handler.
@@ -16,6 +18,35 @@ class UpdatesController extends Controller
     public function updates(): string
     {
         $updates = Telegram::commandsHandler(true);
+
+        foreach ($updates as $update) {
+            if ($update['text'] and $update['from']['id']) {
+                $user = User::find($update['from']['id']);
+
+                if ($user) {
+                    $response = self::addProfile($user, $update['text']);
+                    if ($response['status']) {
+                        Telegram::sendMessage([
+                            'chat_id' => $update['from']['id'],
+                            'text'    => "🤖 `OK! {$response['message']}`",
+                        ]);
+                    }
+                    else {
+                        Telegram::sendMessage([
+                            'chat_id' => $update['from']['id'],
+                            'text'    => "🤖 `ERROR! {$response['message']}`",
+                        ]);
+                    }
+
+                } else {
+                    Telegram::sendMessage([
+                        'chat_id' => $update['from']['id'],
+                        'text'    => "🤖 `I can't find your account. You have to subscribe on " . env('APP_URL') . "`",
+                    ]);
+                }
+            }
+        }
+
         return 'ok';
     }
 }
