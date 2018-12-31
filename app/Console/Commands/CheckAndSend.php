5d53bbe6cadb954ed0c907cea185c1232d153f96
Lorenzo Breda <lorenzo@lbreda.com>
2018-12-31 16:19:34 +0100

    :loud_sound: Logging degli errori

--- app/Console/Commands/CheckAndSend.php
+++ app/Console/Commands/CheckAndSend.php
@@ -69,10 +69,26 @@ class CheckAndSend extends Command
                     if ($this->option('verbose')) {
                         $this->error("Deleted {$instagram_profile->instagram_id} ({$instagram_profile->name}): not found");
                     }
+                } else {
+                    $instagram_profile->last_error = [
+                        'date'      => Carbon::now(),
+                        'exception' => $e->getMessage(),
+                    ];
+                    $instagram_profile->save();
                 }
             } catch (ServerException $e) {
+                $instagram_profile->last_error = [
+                    'date'      => Carbon::now(),
+                    'exception' => $e->getMessage(),
+                ];
+                $instagram_profile->save();
                 $response = null;
             } catch (RequestException $e) {
+                $instagram_profile->last_error = [
+                    'date'      => Carbon::now(),
+                    'exception' => $e->getMessage(),
+                ];
+                $instagram_profile->save();
                 $response = null;
             }
 
